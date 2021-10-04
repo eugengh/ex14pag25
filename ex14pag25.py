d3eabc95b5044fb333fd93fa13e15f52a4485243
eugengh <70998165+eugengh@users.noreply.github.com>
2021-10-04 20:01:54 +0300

    Add files via upload

---
+++ ex14pag25.py
@@ -0,0 +1,45 @@
+"""
+Elaboraţi un program care citeşte de la tastatură un tablou pătratic cu n linii,
+2 ≤ n ≤ 10, şi afişează la ecran suma componentelor care se află:
+	 a)	 pe diagonala principală;
+	 b)	 pe diagonala secundară;
+	 c)	 mai sus de diagonala principală;
+	 d)	 mai jos de diagonala principală;
+	 e)	 mai sus de diagonala secundară;
+	 f)	 mai jos de diagonala secundară.
+"""
+l = int(input("randuri: ")) 
+c = l
+
+matrix = [] 
+print("indicati elementele tabloului")   
+for i in range(l):        
+    a =[] 
+    for j in range(c):    
+         a.append(int(input())) 
+    matrix.append(a) 
+for i in range(l): 
+    for j in range(c): 
+        print(matrix[i][j], end = " ") 
+    print() 
+for e in range(len(matrix[0])):
+    print(matrix[e][e])
+d = (matrix[::-1])
+for q in range(len(d[0])):
+    print(d[q][q])
+for g in range(len(matrix)):
+    for h in range(len(matrix)):
+        if g-h < 0:
+            print(matrix[g][h])
+for b in range(len(matrix)):
+    for n in range(len(matrix)):
+        if b-n > 0:
+            print(matrix[b][n])
+for c in range(len(d)):
+    for v in range(len(d)):
+        if c-v < 0:
+            print(d[c][v])
+for k in range(len(d)):
+    for l in range(len(d)):
+        if k-l > 0:
+            print(d[k][l])
